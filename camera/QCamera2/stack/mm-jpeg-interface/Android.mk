74ec786e3f907c25705b0f86e4f0e76101d3d477
Vijay Venkatraman <vijaykv@google.com>
2017-06-02 16:02:20 -0700

    msm8996: Add libutils to libqomx_core and camera modules
    Bug: 33241851
    Test: build target and pass CTS
    Test: With BOARD_VNDK_VERSION, mma works
    Change-Id: Ibb107bde19cd26675c41059a8c21d5f1f8ffa6bb

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -62,7 +62,7 @@ LOCAL_SRC_FILES := \
 
 LOCAL_MODULE           := libmmjpeg_interface
 LOCAL_PRELINK_MODULE   := false
-LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libqomx_core libmmcamera_interface
+LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libqomx_core libmmcamera_interface libutils
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_32_BIT_ONLY := $(BOARD_QTI_CAMERA_32BIT_ONLY)
