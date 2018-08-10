643d55284d5efc0a400f65ff9c579f3d3b1c308e
LuK1337 <priv.luk@gmail.com>
2018-08-10 17:13:28 +0200

    msm8996: Switch to vendor-minimal sepolicy
    Change-Id: Ic387e62de0750dac798fd1975607dbbef21d93a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,9 +218,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_smartisan
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor-minimal
 
 # Shims
 TARGET_LD_SHIM_LIBS += /vendor/lib/hw/camera.msm8996.so|/vendor/lib/libshim_camera.so
