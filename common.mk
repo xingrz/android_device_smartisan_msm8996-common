4899d1042bb96cdc953ef686be47a4308e1c61b4
XiNGRZ <chenxingyu92@gmail.com>
2019-01-04 04:41:11 +0800

    msm8996: Introduce custom fingerprint@2.0 service
    From hardware/mokee/interfaces/biometrics/fingerprint/2.0
    
    Change-Id: I0fdfb8dbf4ca5d9eae7322780270c6d5eb16649e

--- common.mk
+++ common.mk
@@ -201,7 +201,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service.smartisan
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
