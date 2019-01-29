ec6b8cc5ad3aae16fc3c6535f28f59129c39ac81
XiNGRZ <chenxingyu92@gmail.com>
2019-01-30 00:19:16 +0800

    msm8996: Kang adreno blobs tag "LA.UM.7.5.r1-03700-8x96.0"
    Change-Id: Ie0decb90b1e5c048a407f39437c95268065b2426

--- common.mk
+++ common.mk
@@ -174,8 +174,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor \
+    vendor.display.color@1.0-impl \
+    vendor.display.color@1.0-service \
     copybit.msm8996 \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
